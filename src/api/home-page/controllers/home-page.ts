c8f136ee8f515e92292eac1f6eb8f344e5c19fb8
minhle <duyminh2032000@gmail.com>
2024-06-26 14:18:27 +0700

    feat: fix something

--- src/api/home-page/controllers/home-page.ts
+++ src/api/home-page/controllers/home-page.ts
@@ -2,73 +2,77 @@
  * home-page controller
  */
 
-import { factories } from '@strapi/strapi'
-import { debug } from '../../../utils/index';
-export default factories.createCoreController('api::home-page.home-page', ({ strapi }) => ({
-  async find(ctx) {
+import { factories } from "@strapi/strapi";
+export default factories.createCoreController(
+  "api::home-page.home-page",
+  ({ strapi }) => ({
+    async find(ctx) {
       const { query } = ctx;
-      const entity = await strapi.entityService.findMany('api::home-page.home-page', {
+      const entity = await strapi.entityService.findMany(
+        "api::home-page.home-page",
+        {
           ...query,
           populate: {
             banner: {
-                fields: ["caption","url"],
+              fields: ["caption", "url"],
             },
             new_product: {
               populate: {
-                  category: {
-                    fields: ["name","slug"],
-                    populate: {
-                      products: {
-                        fields: ["name","slug", "price", "promotion_price"],
-                        filter: {
-                          isShow: true,
-                          is_parent: true
+                category: {
+                  fields: ["name", "slug"],
+                  populate: {
+                    products: {
+                      fields: ["name", "slug", "price", "promotion_price"],
+                      filter: {
+                        isShow: true,
+                        is_parent: true,
+                      },
+                      populate: {
+                        avatar: {
+                          fields: ["caption", "url"],
                         },
-                        populate:  {
-                          avatar: {
-                            fields: ["caption","url"],
+                        gallery: {
+                          fields: ["caption", "url"],
+                        },
+                        children_product: {
+                          fields: ["name", "slug", "price", "promotion_price"],
+                          filter: {
+                            isShow: true,
+                            is_parent: false,
                           },
-                          gallery: {
-                            fields: ["caption","url"],
-                          },
-                          children_product: {
-                            fields: ["name","slug", "price", "promotion_price"],
-                            filter: {
-                              isShow: true,
-                              is_parent: false
+                          populate: {
+                            avatar: {
+                              fields: ["caption", "url"],
                             },
-                            populate:  {
-                              avatar: {
-                                fields: ["caption","url"],
-                              }
-
-                            }
-                          }
-                        }
-                      }
-                    }
+                          },
+                        },
+                      },
+                    },
                   },
-                  image: {
-                    fields: ["caption","url"],
-                  }
-              }
+                },
+                image: {
+                  fields: ["caption", "url"],
+                },
+              },
             },
             special_product: {
               populate: {
-                  product: {
-                    populate:  {
-                      avatar: {
-                        fields: ["caption","url"],
-                      },
-                      gallery: {
-                        fields: ["caption","url"],
-                      }
-                    }
-                  }
-              }
-            }
-          }
-      });
+                product: {
+                  populate: {
+                    avatar: {
+                      fields: ["caption", "url"],
+                    },
+                    gallery: {
+                      fields: ["caption", "url"],
+                    },
+                  },
+                },
+              },
+            },
+          },
+        }
+      );
       return this.transformResponse(entity);
-  }
-}));
+    },
+  })
+);
