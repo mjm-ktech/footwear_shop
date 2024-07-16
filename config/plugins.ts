6f067420e7aa9c3a52dec937ae6e8f11347735a0
minhle <duyminh2032000@gmail.com>
2024-07-16 09:09:29 +0700

    feat: reset password

--- config/plugins.ts
+++ config/plugins.ts
@@ -109,4 +109,22 @@ export default ({ env }) => ({
       security: [ { bearerAuth: [] } ]
     }
   },
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST', 'smtp.example.com'),
+        port: env('SMTP_PORT', 587),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+        // ... any custom nodemailer options
+      },
+      settings: {
+        defaultFrom: '18521102@gm.uit.edu.vn',
+        defaultReplyTo: '18521102@gm.uit.edu.vn',
+      },
+    },
+  },
 });
--- src/api/order/controllers/order.ts
+++ src/api/order/controllers/order.ts
@@ -1,7 +1,74 @@
 /**
  * order controller
  */
-
+import utils from "@strapi/utils";
+const { ApplicationError, ValidationError, ForbiddenError } = utils.errors;
 import { factories } from '@strapi/strapi'
+export default factories.createCoreController('api::order.order', ({strapi}) => ({
+  async create(ctx) {
+    try {
+      const { id } = ctx.state.user;
+      strapi.log.info(`start create order with user: ${id}`);
+      const body = ctx.request.body.data;
+      const { items, voucher } = ctx.request.body.data;
 
-export default factories.createCoreController('api::order.order');
+      if (!id) {
+        return ctx.badRequest(
+          "not_login",
+          "need to login to using this feature"
+        );
+      }
+
+      let errorItems = [];
+      body.total = 0;
+      let totalDiscount = 0;
+
+      if (errorItems.length > 0) {
+        throw new ValidationError(
+          `product not found with id ${errorItems}`,
+          "Bad request"
+        );
+      }
+      delete body.items;
+      body.user = {
+        id: id,
+      };
+
+      // phi van chuyen mac dinh
+      body.total += 35000;
+      body.discount = totalDiscount;
+      const order = await strapi.entityService.create("api::order.order", {
+        data: body,
+      });
+
+      items.map(async (item) => {
+        await strapi.entityService.create(
+          "api::product-detail.product-detail",
+          {
+            data: {
+              order: {
+                id: order.id,
+              },
+              product: {
+                id: item.product_id,
+              },
+              quanity: item.quanity,
+              unit_price: item.unit_price,
+            },
+          }
+        );
+      });
+      return {
+        order: order
+      };
+    } catch (e) {
+      strapi.log.error(`An error occurred in create: ${e.message}`);
+      return ctx.badRequest(e.message);
+    }
+  },
+
+
+
+
+
+}));
