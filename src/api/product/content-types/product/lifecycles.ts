395adf282b271eca38a637f6def369d81c7085d6
minhle <duyminh2032000@gmail.com>
2024-09-06 14:47:18 +0700

    fix: update promotion+price

--- src/api/product/content-types/product/lifecycles.ts
+++ src/api/product/content-types/product/lifecycles.ts
@@ -2,7 +2,7 @@ import { debug } from '../../../../utils'
 export default {
   async afterCreate(event) {
     const { result, params } = event;
-    let { height, id } = result;
+    let { height, id, promotion_price, price } = result;
     if(!height) {
       height = 0;
     }
@@ -16,11 +16,20 @@ export default {
     if (height >= 7.6) {
       height_type = "high"
     }
+
     await strapi.entityService.update("api::product.product", id, {
       data: {
         height_type: height_type
       }
-    })
+    });
+
+    if (!promotion_price || promotion_price === 0) {
+      await strapi.entityService.update("api::product.product", id, {
+        data: {
+          promotion_price: price
+        }
+      });
+    }
     // do something to the result;
   },
   async afterFindOne(event) {
