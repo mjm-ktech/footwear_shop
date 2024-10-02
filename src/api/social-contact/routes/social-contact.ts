8702b23839941b713a8c2fa8828c3134ec32e636
minhle <duyminh2032000@gmail.com>
2024-10-02 09:06:49 +0700

    fix

---
+++ src/api/social-contact/routes/social-contact.ts
@@ -0,0 +1,7 @@
+/**
+ * social-contact router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::social-contact.social-contact');
---
+++ src/api/social-contact/services/social-contact.ts
@@ -0,0 +1,7 @@
+/**
+ * social-contact service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::social-contact.social-contact');
